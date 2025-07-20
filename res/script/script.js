3ef7457895c9709ae2231e47aa0426bb98e3d5f6
PedroRomeraDev <168337717+PedroRomeraDev@users.noreply.github.com>
2025-07-20 16:37:48 +0200

    eroland

--- res/script/script.js
+++ res/script/script.js
@@ -1,4 +1,18 @@
-function descargar() {
+function downloadNormal() {
+    window.location.href = "https://drive.google.com/uc?export=download&id=1Awhji6s5YZHXgwyARP_SDKby3ECPNfNB";
+  }
+function downloadCurseForge() {
+    window.location.href = "https://drive.google.com/uc?export=download&id=1VaibQm0Bh5bVQKOdfbtpcOxEpflXA2Eh";
+  }
+function downloadInstaller() {
+    window.location.href = "https://drive.google.com/uc?export=download&id=1LjJ86V9IAMvP1lyt0-oo0IiR3SieKH9v";
+  }
+function tutorialNormal() {
     window.location.href = "https://www.google.es";
   }
-  
+function tutorialCurseForge() {
+    window.location.href = "https://www.google.es";
+  } 
+function tutorialInstaller() {
+    window.location.href = "https://www.google.es";
+  } 
